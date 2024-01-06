a979ff96d51fe0f12c3a506f7fddd520de07898d
Hank-max <76566040+Hank-max@users.noreply.github.com>
2024-01-06 16:18:12 -0500

    Making python tool to move files
    making python file to move files.

---
+++ BJ_data_files/MovingFilesFromQfit.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 16:10:00 2024
+
+@author: henry
+"""
+"Thsi can only be ran on laptop because of the file directories."
+
+import os
+import shutil
+
+source_dir = 'C:/ProgramData/qfit/cvdata'
+target_dir = 'C:/Users/henry_ah1kl9o/OneDrive/Docu'
+
+for file_name in os.listdir(source_dir):
+    if file_name.endswith('.csv'):
+        shutil.move(os.path.join(source_dir, file_name), target_dir)
